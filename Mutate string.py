115ab32e6c9a970673ffcd7345eeb6c7a0cee22d
Abirham_Gedefaw <gedefawabirham@gmail.com>
2026-02-04 09:43:54 +0300

    mutate a string

--- Mutate string.py
+++ Mutate string.py
@@ -5,3 +5,14 @@ def mutate_string(string, position, character):
     return "".join(l)
 
 if __name__ == '__main__':
+    
+""" 
+The above code have o(n) time and space complexity
+But the following code is the optimized one with o(n) time complexity and o(1) space complexity 
+    
+"""
+    
+def mutate_string(string, position, character):
+    return string[:position] + character + string[position+1:]
+
+if __name__ == '__main__':
