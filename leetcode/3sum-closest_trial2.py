1d61e7a8f8b0a1b092e0f40d883392e218955c0d
Abirham_Gedefaw <gedefawabirham@gmail.com>
2026-03-09 17:54:22 +0300

    Add solution for 3Sum Closest (trial 2)

---
+++ leetcode/3sum-closest_trial2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        closest_sum = float("inf")
+        for i in range(len(nums) - 2):
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if abs(target - total) < abs(target - closest_sum):
+                    closest_sum = total
+                if total == target:
+                    return total
+                elif total < target:
+                    left += 1
+                else:
+                    right -= 1
+        return closest_sum
+
+        
+        
